4dd6d2c92d9b0858985160687f56b063b8609cd9
nozakikyouta <tuebkbxhmb@icloud.com>
2020-09-15 16:30:39 +0900

    devise設定とカラムの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :move_to_index, except: [:index, :show]
 
   private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASICT_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
