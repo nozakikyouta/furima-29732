015d4ed4b2fdc9525aa5dab4fa3e35cfd03886a4
nozakikyouta <tuebkbxhmb@icloud.com>
2020-09-24 14:49:52 +0900

    activeHash

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,4 +18,5 @@ class ItemsController < ApplicationController
   private
   def message_params
     params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,28 @@ class Item < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         extend ActiveHash::Associations::ActiveRecordExtensions
+         belongs_to_active_hash :category
+         belongs_to_active_hash :condition
+         belongs_to_active_hash :prefecture
+         belongs_to_active_hash :schedule_item
+         belongs_to_active_hash :shopping_fee
+
+         validates :category, presence: true
+         validates :condition, presence: true
+         validates :prefecture, presence: true
+         validates :schedule_item, presence: true
+         validates :shopping_fee, presence: true
+         validates :category, numericality: { other_than: 0 }
+         validates :condition, numericality: { other_than: 0 } 
+         validates :prefecture, numericality: { other_than: 0 }
+         validates :schedule_item, numericality: { other_than: 0 }
+         validates :shopping_fee, numericality: { other_than: 0 }
 
          belongs_to :user
          has_one_attached :image
 
-          with_options presence: true do
+        with_options presence: true do
           validates :name
           validates :category_id
           validates :condition_id
