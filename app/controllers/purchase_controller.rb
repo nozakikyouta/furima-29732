a1d935ee087f24caede531847dff15ec78b4a419
nozakikyouta <tuebkbxhmb@icloud.com>
2020-10-02 14:35:10 +0900

    商品詳細表示機能

---
+++ app/controllers/purchase_controller.rb
@@ -0,0 +1,7 @@
+class PurchaseController < ApplicationController
+  def index
+    
+  end
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items,only: [:index, :new, :update, :create, :show, :edit, :destroy] do
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
+  resources :purchase,only: [:index,:new]do
   end
 end
 
