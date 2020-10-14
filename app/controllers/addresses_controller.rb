05bd48e60848d862e22e78f91ded08b75e8e0d36
nozakikyouta <tuebkbxhmb@icloud.com>
2020-10-14 17:58:46 +0900

    修正

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,17 +1,2 @@
 class AddressesController < ApplicationController
-
-  # def create
-  #   @address = Adress.create(address_params)
-  #   if @address.save
-  #     redirect_to root_path
-  #   else
-  #     render action: :index
-  #   end
-  # end
-
-  # private
-
-  # def address_params
-  #   params.require(:address).permit(:prefecture_id :municipality :address :building_name :phone_number)
-  # end
 end
