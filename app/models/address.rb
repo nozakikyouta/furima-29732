c0dac0402c03cd330acdc7218a38980e275297f9
nozakikyouta <tuebkbxhmb@icloud.com>
2020-10-14 18:39:32 +0900

    修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,11 +3,4 @@ class Address < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to:purchase
 
-  with_options presence: true do
-    validates :postal_code,format:{with:(/\A\d{3}[-]\d{4}\z/),message:""}
-    validates :municipality
-    validates :address
-    validates :building_name
-    validates :phone_number ,format:{with:/\A\d{11}\z/,message: ""}
-  end
 end
